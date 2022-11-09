b72f39ebc993849bc83d0dad6a536a3bce6ce002
Xyphyn <xyphyn@outlook.com>
2022-11-08 17:17:02 -0800

    Updated gradle settings to match new name

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "us.xylight.surveyer"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 
-rootProject.name = "Discord"
+rootProject.name = "Surveyer"
 
