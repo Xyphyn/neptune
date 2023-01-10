89a63fd73fc08a705ab318da66a68c22a8cf8765
Xyphyn <xyphyn@outlook.com>
2023-01-10 09:22:32 -0800

    Add a logger

---
+++ src/main/kotlin/us/xylight/neptune/Logger.kt
@@ -0,0 +1,21 @@
+package us.xylight.neptune
+
+import us.xylight.neptune.util.Color
+
+enum class LogLevel(val priority: Int, val color: String) {
+    ERROR(1, Color.RED_BRIGHT),
+    WARNING(2, Color.YELLOW),
+    INFO(3, Color.BLUE),
+    VERBOSE(4, Color.BLUE_BRIGHT),
+    DEBUG(5, Color.GREEN)
+}
+
+object Logger {
+    var logLevel = LogLevel.INFO
+
+    fun log(message: String, level: LogLevel) {
+        if (level.priority <= logLevel.priority) {
+            println("${level.color} [${level.name}] $message ${Color.RESET}")
+        }
+    }
+}
