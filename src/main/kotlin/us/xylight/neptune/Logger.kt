6e527ccadeccbab6bdc8bd116b35d2a3aefda38b
Xyphyn <xyphyn@outlook.com>
2023-01-10 14:28:53 -0800

    Update logger to make colors more pleasing

--- src/main/kotlin/us/xylight/neptune/Logger.kt
+++ src/main/kotlin/us/xylight/neptune/Logger.kt
@@ -2,12 +2,12 @@ package us.xylight.neptune
 
 import us.xylight.neptune.util.Color
 
-enum class LogLevel(val priority: Int, val color: String) {
-    ERROR(1, Color.RED_BRIGHT),
-    WARNING(2, Color.YELLOW),
-    INFO(3, Color.BLUE),
-    VERBOSE(4, Color.BLUE_BRIGHT),
-    DEBUG(5, Color.GREEN)
+enum class LogLevel(val idColor: String, val textColor: String) {
+    ERROR(Color.RED_BOLD_BRIGHT, Color.RED_BRIGHT),
+    WARNING(Color.YELLOW_BOLD_BRIGHT, Color.YELLOW_BRIGHT),
+    INFO(Color.BLUE_BOLD, Color.BLUE),
+    VERBOSE(Color.BLUE_BOLD_BRIGHT, Color.BLUE_BRIGHT),
+    DEBUG(Color.GREEN_BOLD_BRIGHT, Color.GREEN_BRIGHT)
 }
 
 object Logger {
@@ -15,8 +15,8 @@ object Logger {
     var logLevel = LogLevel.VERBOSE
 
     fun log(message: String, level: LogLevel) {
-        if (level.priority <= logLevel.priority) {
-            println("${level.color} [${level.name}] $message ${Color.RESET}")
+        if (level.ordinal <= logLevel.ordinal) {
+            println("[${level.idColor}${level.name}${Color.RESET}] ${level.textColor}$message${Color.RESET}")
         }
     }
 }
